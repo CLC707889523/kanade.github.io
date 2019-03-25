05701e974b4e7d8d5a59954c38bcb765c7e88a51
CLC707889523 <chenlicong0418@hotmail.com>
2019-03-25 17:31:58 +0800

    push configuration

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <div id = "app">
+      <h4>Welcome to kanade's blog!</h4>
+    </div>
+    <el-row id = "infomation">
+      <el-col :span="24">这个项目采用了Vue+ElementUI+Express+Node+Webpack+mongoose</el-col>
+      <el-col :span="24">另外还有jsonwebtoken+bcrypt的跨域用户信息服务器验证</el-col>
+    </el-row>
+    <el-row id = "button-center" >
+      <el-button @click="clickButton()" type = "success">点我</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+
+  methods: {
+    clickButton () {
+      console.log('触发成功')
+    }
+  }
 }
+
 </script>
 
 <style>
@@ -19,5 +34,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#infomation {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: blue;
+  margin-top: 120px;
+}
+
+#button-center {
+  text-align: center;
+  margin: 120px 0px
 }
 </style>
